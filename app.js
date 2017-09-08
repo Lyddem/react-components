c95f3729f3fc2c8fa6304c68942f3a9dac427c65
Lyddem <lydiademi@gmail.com>
2017-09-08 15:13:23 -0700

    Rendering using props

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
+
+var GroceryListItem = (props) => (
+  <div>
+      <ul>
+        <li>{props.groceries[0]}</li>
+        <li>{props.groceries[1]}</li>
+      </ul>
+  </div>
+);
+
 var GroceryList = () => (
   <div>
-    <Tea />
-    <Bread />
+    <h2> My Groceries </h2>
+      <GroceryListItem groceries={['tea', 'bread']}/>
   </div>
 );
 
-var Bread = () => (
-  <div>
-    <ul>
-      <li>Bread</li>
-    </ul>
-  </div>
-);
-
-var Tea = () => (
-  <div>
-    <ul>
-      <li>Tea</li>
-    </ul>
-  </div>
-);
 ReactDOM.render(<GroceryList />, document.getElementById("app"));
+
+
+// var Bread = () => (
+//   <div>
+//     <ul>
+//       <li>Bread</li>
+//     </ul>
+//   </div>
+// );
+
+// var Tea = () => (
+//   <div>
+//     <ul>
+//       <li>Tea</li>
+//     </ul>
+//   </div>
+// );
+
+
+
