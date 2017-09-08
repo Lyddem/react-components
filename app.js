77e1d9400d7b617c9e923139a45a675fb23e97f4
Lyddem <lydiademi@gmail.com>
2017-09-08 14:43:29 -0700

    First render

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// TODO
+var GroceryList = () => (
+  <div>
+    <Tea />
+    <Bread />
+  </div>
+);
+
+var Bread = () => (
+  <div>
+    <ul>
+      <li>Bread</li>
+    </ul>
+  </div>
+);
+
+var Tea = () => (
+  <div>
+    <ul>
+      <li>Tea</li>
+    </ul>
+  </div>
+);
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
